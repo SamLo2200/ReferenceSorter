2efe6ce3ee5a276162534b7ff76a87ff500335ee
Sam <144938568+SamLo2200@users.noreply.github.com>
2024-10-25 23:08:20 +0800

    added handleChange function

--- src/components/DymanicTable.tsx
+++ src/components/DymanicTable.tsx
@@ -4,38 +4,41 @@ import { useState } from "react";
 import { Input } from "@/components/ui/input";
 
 export default function DymanicTable() {
-  const [citationContent] = useState<string[]>([
-    "test1",
-    "test2",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-    "test3",
-  ]);
-  // const [citationCount, setCitationCount] = useState<number>(0);
+    const [citationContent, setCitationContent] = useState<string[]>([
+        "test1",
+        "test2",
+        "test3",
+    ]);
+    // const [citationCount, setCitationCount] = useState<number>(0);
 
-  return (
-    <div>
-      {citationContent.map((citation: string, index: number) => {
-        return (
-          <div className="citationInsert flex flex-col my-5" key={index}>
-            <div>
-              <label>Citation {index + 1}</label>
-              <Input placeholder="Enter your citation here" />
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+    const handleChange = (
+        index: number,
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const newCitationContent = [...citationContent];
+        newCitationContent[index] = event.target.value;
+        setCitationContent(newCitationContent);
+        console.log(newCitationContent);
+    };
+
+    return (
+        <div>
+            {citationContent.map((citation: string, index: number) => {
+                return (
+                    <div
+                        className="citationInsert flex flex-col my-5"
+                        key={index}>
+                        <div>
+                            <label>Citation {index + 1}</label>
+                            <Input
+                                placeholder="Enter your citation here"
+                                name="citation"
+                                onChange={(e) => handleChange(index, e)}
+                            />
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
