1682454424dab55dc7e302a0d6113d66f710ea92
Sam <144938568+SamLo2200@users.noreply.github.com>
2024-10-25 15:20:46 +0800

    First Commit

---
+++ src/components/DymanicTable.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+
+export default function DymanicTable() {
+  const [citationContent, setCitationContent] = useState<string[]>([
+    "test1",
+    "test2",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+    "test3",
+  ]);
+  const [citationCount, setCitationCount] = useState<number>(0);
+
+  return (
+    <div>
+      {citationContent.map((citation: string, index: number) => {
+        return (
+          <div className="citationInsert flex flex-col my-5" key={index}>
+            <div>
+              <label>Citation {index + 1}</label>
+              <Input placeholder="Enter your citation here" />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
